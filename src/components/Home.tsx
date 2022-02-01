05fbe36b54bc374c02fdf9bbe94e49d596e3ecf2
Richard Wright <RWright2@phlexglobal.com>
2022-02-01 15:47:47 +0000

    tweak Home css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,13 +6,23 @@ const Home: React.FC = () => {
     <Center height="100vh">
       <VStack spacing="1.5rem">
         <chakra.img alt="logo" src={logo} width="60%" marginBottom="1rem" />
-        <Text textAlign="center" color="gray.800" fontWeight="600">
+        <Text
+          textAlign="center"
+          color="gray.800"
+          fontWeight="600"
+          padding="0 2rem"
+        >
           Some fascinating introductory text about the PhlexEtrack app.
         </Text>
         <Button background="primary" color="white">
           Help Me
         </Button>
-        <Text textAlign="center" color="gray.800" fontWeight="600">
+        <Text
+          textAlign="center"
+          color="gray.800"
+          fontWeight="600"
+          padding="0 2rem"
+        >
           Click on the Scan Document tab below to scan a document barcode.
         </Text>
       </VStack>
