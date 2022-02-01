135e6b6f66ff3f26efe860ba9fb6a4f51677b00a
Richard Wright <RWright2@phlexglobal.com>
2022-02-01 15:37:16 +0000

    flesh out Home and ScanResult

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,20 @@
-import { Center, VStack } from "@chakra-ui/react";
+import { Center, VStack, Text, Button, chakra } from "@chakra-ui/react";
 import logo from "../img/logo.png";
 
 const Home: React.FC = () => {
   return (
     <Center height="100vh">
-      <VStack>
-        <img alt="logo" src={logo} width="50%" />
+      <VStack spacing="1.5rem">
+        <chakra.img alt="logo" src={logo} width="60%" marginBottom="1rem" />
+        <Text textAlign="center" color="gray.800" fontWeight="600">
+          Some fascinating introductory text about the PhlexEtrack app.
+        </Text>
+        <Button background="primary" color="white">
+          Help Me
+        </Button>
+        <Text textAlign="center" color="gray.800" fontWeight="600">
+          Click on the Scan Document tab below to scan a document barcode.
+        </Text>
       </VStack>
     </Center>
   );
--- src/components/ScanResult.tsx
+++ src/components/ScanResult.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
-import { Center, Text, VStack, Box, useTheme } from "@chakra-ui/react";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { Center, Text, VStack } from "@chakra-ui/react";
+import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ScanResult: React.FC = () => {
@@ -11,6 +11,9 @@ const ScanResult: React.FC = () => {
     <Center height="100vh">
       <VStack spacing="2rem">
         <FontAwesomeIcon icon={faCheckCircle} color="#68D391" size="6x" />
+        <Text fontSize="1.5rem" fontWeight="700">
+          Document Scanned
+        </Text>
         <VStack background="gray.100" padding="1rem" borderRadius="0.5rem">
           <Text fontWeight="600" color="gray.700">
             Barcode
