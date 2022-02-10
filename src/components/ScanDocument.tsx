b5f526585b1634d9fe2a8b7efa3edf9d18dd7660
Richard Wright <RWright2@phlexglobal.com>
2022-02-10 09:34:20 +0000

    Turn on torch

--- src/components/ScanDocument.tsx
+++ src/components/ScanDocument.tsx
@@ -36,7 +36,7 @@ const ScanDocument: React.FC = () => {
         />
       </Center>
       <Center height="100vh" paddingBottom="3rem" background="#000" zIndex="0">
-        <ScanningApp onUpdate={handleScan} />
+        <ScanningApp onUpdate={handleScan} torch />
       </Center>
     </>
   );
