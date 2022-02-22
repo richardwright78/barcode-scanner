b82dc6426db7970dd46851c85a79f3534f966932
Richard Wright <RWright2@phlexglobal.com>
2022-02-22 11:06:32 +0000

    use .env

--- src/components/ScanDocument.tsx
+++ src/components/ScanDocument.tsx
@@ -10,8 +10,7 @@ const ScanDocument: React.FC = () => {
   const [documentScanned, setDocumentScanned] = useState<boolean>(false);
   const navigate = useNavigate();
 
-  const licenseKey =
-    "AXlhdAW1JMTrESs17z800ZoOakxsK4wIiD/2ZANEOC0xSV0uzXMEllVhIisZfep6sneH/dJENNFVH6VglxK27CNqtfoPWEhcjSh0xCtlMTL/Ln9mwg1yzyYdxhv6ONpjaa5nRbOYgDM03km/wY5kt/xeZYk9dU7TX1+44SSSRMs+uF2B66UlDjw/vqf2io4UQdHejf8FFCygbs0AfijI87+JRyxwdYpdSz3BWXH1ww+bSiRV79UiaTzKBCsxlCmEq6VNa/kCDuA1DYSlMBY3GlRH6rUom2GcA3qAm5oAIzQEgEHI1sMoRDz8+WoD15zYQfxxVYl7b06kpqkXEC69MicGbIRYhtcqplrJBdI/4SueiwsZglvEloZmFqNf5lsEJzNMllOqzoU2daVOdP9L7XkdYC9CEPgIYZ8c5G52Bd97s0UYKPIWgUhYAK1XOdFPKFBG4YtJbvHtW+bU8APO5QuraXSg5jSyuC6eHK87sK3ppRX8lzuLOpO5E5I7m4VAXhIPZr+JBeCw18NILV9DRJeqQf5Cg9gTcyNX00yFNZuz52yQ/MyjqLcW4TCu3BPoUKpH2gsYxMqhf+xbj2rPnJjk9epffQv8WMn3c93b84xjdb5IBkKKe8Nx1uKKnbGxt4oFyPN8OpDjjfRcmC2zwB3TVkmcfdA6s5q1EFxOuBznprZTJOivtUcusPf1gfPdAInVvZiwsPmN6k9TNbQ95289COI9DN0ZqmDOpzL78P+LQIRSu726l57sozxbfcFQQxJIb89i1MQUxyoSXcuLgKJ6vPY0hchOukmpN3Wkig==";
+  const licenseKey = process.env.REACT_APP_SCANDIT_KEY;
 
   const scanSettings = new ScanSettings({
     enabledSymbologies: [Barcode.Symbology.CODE39],
